b1c3e19d7f0be1e58e3153f01da06e73c2424e63
Joao Saavedra <joao_saavedra_18@hotmail.com>
2023-07-12 19:53:29 +0100

    displaying posts in feeds with infinite scrolling not fully implemented

--- src/components/MiniCreatePost.tsx
+++ src/components/MiniCreatePost.tsx
@@ -32,18 +32,18 @@ const MiniCreatePost: FC<MiniCreatePostProps> = ({ session }) => {
 
         <Input
           readOnly
-          onClick={() => router.push(pathname + '/submit)')}
+          onClick={() => router.push(pathname + '/submit')}
           placeholder='Create post'
         />
 
         <Button
-          onClick={() => router.push(pathname + '/submit)')}
+          onClick={() => router.push(pathname + '/submit')}
           variant='ghost'>
           <ImageIcon className='text-zinc-600' />
         </Button>
 
         <Button
-          onClick={() => router.push(pathname + '/submit)')}
+          onClick={() => router.push(pathname + '/submit')}
           variant='ghost'>
           <Link2 className='text-zinc-600' />
         </Button>
